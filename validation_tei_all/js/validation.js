daf096f63baf333dc6974fd0d109a78d42a8e333
Ashley M. Clark <amclark42@gmail.com>
2017-11-10 10:32:08 -0500

    Add collapsable headings
    The JS code from Hieractivity (and HeydonWorks before that) is
    more-or-less the same, but the styling on the expansion icons has
    been changed to reflect an updated tutorial from HeydonWorks at
    https://inclusive-components.design/collapsible-sections

--- validation_tei_all/js/validation.js
+++ validation_tei_all/js/validation.js
@@ -1,3 +1,26 @@
 $(document).ready(function() {
   console.log('Hello world');
+  
+  // The following code is adapted with few changes from hieractivity.js, which in
+    // turn adapted it from HeydonWorks. It should probably be moved to a new JS 
+    // library in the common folder.
+  $('.collapsableHeading').each(function() {
+    var heading = $(this),
+        collapsable = heading.next(),
+        collapsableID = collapsable.attr('id');
+    //console.log(collapsableID);
+    var isAutoCollapsed = collapsable.is('[class~=expandable-hidden]');
+    heading.wrapInner('<button aria-expanded="'+ !isAutoCollapsed
+      +'" aria-controls="'+ collapsableID +'"></button>');
+    collapsable.attr('aria-hidden', isAutoCollapsed);
+    var button = heading.children('button');
+    // Show or hide the expandable <div> when the associated button is pressed.
+    button.on('click', function() {
+      var isExpanding = $(this).attr('aria-expanded') === 'false' ? true : false;
+      collapsable.slideToggle();
+      collapsable.toggleClass('expandable-hidden');
+      collapsable.attr('aria-hidden', !isExpanding);
+      $(this).attr('aria-expanded', isExpanding);
+    });
+  });
 });
