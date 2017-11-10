f7e4e38010502d1b32e1675b920f773e54592931
Ashley M. Clark <amclark42@gmail.com>
2017-11-10 11:40:44 -0500

    Change classname for collapsed sections

--- validation_tei_all/js/validation.js
+++ validation_tei_all/js/validation.js
@@ -9,16 +9,16 @@ $(document).ready(function() {
         collapsable = heading.next(),
         collapsableID = collapsable.attr('id');
     //console.log(collapsableID);
-    var isAutoCollapsed = collapsable.is('[class~=expandable-hidden]');
+    var isAutoCollapsed = collapsable.is('[class~=collapsable-hidden]');
     heading.wrapInner('<button aria-expanded="'+ !isAutoCollapsed
       +'" aria-controls="'+ collapsableID +'"></button>');
     collapsable.attr('aria-hidden', isAutoCollapsed);
     var button = heading.children('button');
-    // Show or hide the expandable <div> when the associated button is pressed.
+    // Show or hide the collapsable <div> when the associated button is pressed.
     button.on('click', function() {
       var isExpanding = $(this).attr('aria-expanded') === 'false' ? true : false;
       collapsable.slideToggle();
-      collapsable.toggleClass('expandable-hidden');
+      collapsable.toggleClass('collapsable-hidden');
       collapsable.attr('aria-hidden', !isExpanding);
       $(this).attr('aria-expanded', isExpanding);
     });
