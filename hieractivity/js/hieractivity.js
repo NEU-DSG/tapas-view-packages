b44ccd0359c7ad72faff49af36eb9e11c4a5156e
Ashley M. Clark <amclark42@gmail.com>
2017-11-14 18:25:48 -0500

    Try 500ms delay

--- hieractivity/js/hieractivity.js
+++ hieractivity/js/hieractivity.js
@@ -116,7 +116,7 @@ $(document).ready(function() {
 
   // Assign box (and control panel) heights soon, and when the window is resized.
   $(window).resize(assignAllHeights);
-  setTimeout(assignAllHeights, 1000);
+  setTimeout(assignAllHeights, 500);
   
   // Make the control panel draggable.
   /*$('#control-panel').draggable({
